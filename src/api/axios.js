38d6e7a9d7b6a8e82f9b1a2d6f90fe5ab24049e1
kiptanuiBoaz <boaserem022@gmail.com>
2023-02-09 22:45:47 +0300

    cookie set issue fix at refreshtoken hook

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const BASE_URL = "http://localhost:3500";
 
 
-//set global baseurl
+//set global baseurls
 export default axios.create({
     baseURL: BASE_URL,
     withCredentials: true,
